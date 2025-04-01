bc9dd28aeb92e2bc6b40c6557d35a9fcb820075a
Fayzullo <92411568+fayzullo1501@users.noreply.github.com>
2025-04-01 11:42:37 +0500

    Update Chat.js

--- backend/src/models/Chat.js
+++ backend/src/models/Chat.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const ChatSchema = new mongoose.Schema(
   {
     course: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true }, // Курс
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },   // Отправитель
-    message: { type: String, required: true },                                        // Сообщение
-    timestamp: { type: Date, default: Date.now },                                     // Время
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Отправитель (учитель/студент)
+    message: { type: String, required: true }, // Текст сообщения
+    timestamp: { type: Date, default: Date.now }, // Время отправки
   },
   { timestamps: true }
 );
 
 const Chat = mongoose.model("Chat", ChatSchema);
-export default Chat;
+module.exports = Chat;
